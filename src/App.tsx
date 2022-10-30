ea534a07fb836c59c3c5634d44c59723a4248fe6
Steven <gonghaima@hotmail.com>
2022-10-30 21:30:05 +1100

    complete solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Select } from './Select';
+import { useState } from 'react';
+import { Select, SelectOption } from './Select';
 
 const options = [
   { label: 'First', value: 1 },
@@ -9,10 +10,19 @@ const options = [
 ];
 
 function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
   return (
     <>
-      {/* <Select options={options} value={label:"fd",value:""} onChange={() => {}} /> */}
-      <Select options={options} onChange={() => {}} />
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
     </>
   );
 }
